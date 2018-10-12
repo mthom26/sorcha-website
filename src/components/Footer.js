86101d401bb82316590102e6aaa09e10d5a2ba4a
mthom26 <mthom2626@gmail.com>
2018-10-12 22:36:22 +0100

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Footer.css';
+import facebook from '../images/facebook-square.svg';
+import youtube from '../images/youtube-square.svg';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footerContainer">
+        <div className="footerLogo">
+          Logo
+        </div>
+        <div className="footerContent">
+          <div className="footerIcons">
+            <a href="#"><img src={facebook} width="24px" /></a>
+            <a href="#"><img src={youtube} width="24px" /></a>
+          </div>
+          <div className="footerText">2018 Sorcha Thompson</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 
 import Nav from './Nav';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <Fragment>
       <Nav />
       {children}
+      <Footer />
     </Fragment>
   );
 };
