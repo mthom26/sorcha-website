1d66418637c30752fbba9fb5a1df1de83e10e549
mthom26 <mthom2626@gmail.com>
2018-10-15 20:41:37 +0100

    Add excerpt and links to blogpost page in blog page

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Blog = ({ data }) => {
-  console.log(data);
+  //console.log(data);
   return (
     <div>
       <h1>Blog Posts</h1>
-      {data.allContentfulBlogPost.edges.map((blogPost, index) => (
-        <div key={index} style={{border: '1px solid green'}}>
-          <h2>{blogPost.node.title}</h2>
-          <span>{blogPost.node.date}</span>
+      {data.allContentfulBlogPost.edges.map(({ node }, index) => {
+        const { title, date, body, slug } = node;
+        
+        return (
+          <div key={index} style={{border: '1px solid green'}}>
+          <h2>{title}</h2>
+          <span>{date}</span>
+          <p>{body.childMarkdownRemark.excerpt}</p>
+          <Link to={`/blog/${slug}`}>Read More</Link>
         </div>
-      ))}
+        )
+      })}
     </div>
   );
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -25,6 +25,11 @@ export const query = graphql`
         title
         slug
         date(formatString: "DD-MM-YYYY")
+        body {
+          childMarkdownRemark {
+            excerpt(pruneLength: 150)
+          }
+        }
       }
     }
   }
