6f21e288d49934edc3a85300800f45d721acf1db
mthom26 <mthom2626@gmail.com>
2018-11-19 21:19:00 +0000

    Add logic to generate links from blog page

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -5,10 +5,21 @@ import Img from 'gatsby-image';
 import './Blog.css';
 import PageHeader from '../PageHeader';
 
+const getPathPrefix = (locale) => {
+  // If locale is 'en' the path should not include it
+  // e.g. '/en/blog' should just return '/blog'
+  if(locale === 'en') {
+    return ''; //empty string
+  }
+  // otherwise return the locale string
+  return locale;
+}
+
 const Blog = ({ data, locale }) => {
   console.log(data);
   const blogPosts = locale === 'en' ? data.englishBlogPosts : data.germanBlogPosts;
-  
+  const localePath = getPathPrefix(locale);
+
   return (
     <div>
       <PageHeader title="Blog" classes="blogHeader" />
@@ -26,7 +37,7 @@ const Blog = ({ data, locale }) => {
               <span>{date}</span>
               <hr />
               <p>{body.childMarkdownRemark.excerpt}</p>
-              <Link to={`/blog/${slug}`}>Read More</Link>
+              <Link to={`/${localePath}/blog/${slug}`}>Read More</Link>
             </div>
           </div>
         )
