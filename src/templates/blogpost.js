73b14323f4953103d6e9918874342a100b2a0f73
mthom26 <mthom2626@gmail.com>
2018-11-19 21:39:03 +0000

    Add logic to send correct data to BlogPost template by locale

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -7,18 +7,33 @@ import locales from '../data/locales';
 import Layout from '../components/Layout';
 import BlogPost from '../components/blogPost/BlogPost';
 
-// !! Need to make sure the pageContext contains locales and other data !!
 const BlogPostPage = ({ data, pageContext }) => {
   
   const { locale } = pageContext;
   const { localeData } = locales[locale];
+  const title = locale === 'en'
+    ? data.mainEnglish.title
+    : data.mainGerman.title;
 
+  // Construct object based on current locale to be passed to BlogPost.js
+  const localisedData = locale === 'en'
+    ? {
+      main: data.mainEnglish,
+      prev: data.prevEnglish,
+      next: data.nextEnglish
+    }
+    : {
+      main: data.mainGerman,
+      prev: data.prevGerman,
+      next: data.nextGerman
+    };
+  
   return (
     <Layout localeData={localeData} locale={locale}>
       <Helmet>
-        <title>{data.main.title} | Sorcha Thompson</title>
+        <title>{title} | Sorcha Thompson</title>
       </Helmet>
-      <BlogPost data={data} pageContext={pageContext} />
+      <BlogPost data={localisedData} pageContext={pageContext} />
     </Layout>
   );
 };
@@ -27,7 +42,21 @@ export default BlogPostPage;
 
 export const query = graphql`
 query BlogPostQuery ($slug: String!, $nextSlug: String, $prevSlug: String) {
-  main: contentfulBlogPost(slug: { eq: $slug }) {
+  mainEnglish: contentfulBlogPost(node_locale: { eq: "en-US"}, slug: { eq: $slug }) {
+    title
+    date
+    body {
+      childMarkdownRemark {
+        html
+      }
+    }
+    coverImage {
+      fluid(maxWidth: 1920) {
+        ...GatsbyContentfulFluid
+      }
+    }
+  }
+  mainGerman: contentfulBlogPost(node_locale: { eq: "de"}, slug: { eq: $slug }) {
     title
     date
     body {
@@ -42,7 +71,17 @@ query BlogPostQuery ($slug: String!, $nextSlug: String, $prevSlug: String) {
     }
   }
 
-  prev: contentfulBlogPost(slug: { eq: $prevSlug }) {
+  prevEnglish: contentfulBlogPost(node_locale: { eq: "en-US"}, slug: { eq: $prevSlug }) {
+    title
+    date
+    slug
+    coverImage {
+      fluid(maxWidth: 1000) {
+        ...GatsbyContentfulFluid
+      }
+    }
+  }
+  prevGerman: contentfulBlogPost(node_locale: { eq: "de"}, slug: { eq: $prevSlug }) {
     title
     date
     slug
@@ -53,7 +92,17 @@ query BlogPostQuery ($slug: String!, $nextSlug: String, $prevSlug: String) {
     }
   }
 
-  next: contentfulBlogPost(slug: { eq: $nextSlug }) {
+  nextEnglish: contentfulBlogPost(node_locale: { eq: "en-US"}, slug: { eq: $nextSlug }) {
+    title
+    date
+    slug
+    coverImage {
+      fluid(maxWidth: 1000) {
+        ...GatsbyContentfulFluid
+      }
+    }
+  }
+  nextGerman: contentfulBlogPost(node_locale: { eq: "de"}, slug: { eq: $nextSlug }) {
     title
     date
     slug
