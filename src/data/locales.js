6bb5fab189c595dd33d383efbdb8b4f05ed58e84
mthom26 <mthom2626@gmail.com>
2018-11-19 14:47:39 +0000

    Add locales file

---
+++ src/data/locales.js
@@ -0,0 +1,30 @@
+module.exports = {
+  en: {
+    path: 'en',
+    locale: 'English',
+    default: true,
+    localeData: {
+      nav: {
+        music: 'music',
+        gigs: 'gigs',
+        gallery: 'gallery',
+        blog: 'blog',
+        contact: 'contact'
+      }
+    }
+  },
+  de: {
+    path: 'de',
+    locale: 'German',
+    default: false,
+    localeData: {
+      nav: {
+        music: 'music_g',
+        gigs: 'gigs_g',
+        gallery: 'gallery_g',
+        blog: 'blog_g',
+        contact: 'contact_g'
+      }
+    }
+  }
+}
