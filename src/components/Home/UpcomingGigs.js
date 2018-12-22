31afcc3d1ad3c6f4c1bc9fb63246ec1a89a6b6a4
mthom26 <mthom2626@gmail.com>
2018-12-22 20:33:18 +0000

    Add formatDate util function and style upcomingGigs entries

--- src/components/Home/UpcomingGigs.js
+++ src/components/Home/UpcomingGigs.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import { formatDate } from '../../utils';
+
+import './UpcomingGigs.css';
+
 const UpcomingGigs = ({ gigs }) => {
   return (
-    <div>
+    <div className="upcomingGigs">
       <h2>Upcoming Gigs</h2>
+      <hr />
       {gigs.map(({ node }) => {
         const gigDate = Date.parse(node.date);
         const now = Date.now();
         if(gigDate > now) {
+          const formattedDate = formatDate(node.date);
           return (
-            <div>
-              <p>{node.date}</p>
-              <p>{node.address}</p>
+            <div className="gigEntry">
+              <span>{formattedDate}</span> <span>{node.address}</span>
             </div>
           );
         } else {
--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,22 @@ export const getPathPrefix = (locale) => {
   // otherwise return the locale string
   return locale;
 };
+
+export const formatDate = (dateString) => {
+  // dateString should be YYYY-MM-DDTHH:MM+HH:MM
+  // Return date of form "DD-MM-YYYY, h:mma"
+  const dateStringSplit = dateString.split('T');
+  const date = dateStringSplit[0].split('-').reverse().join('-');
+  const timeHour = parseInt(dateStringSplit[1].slice(0, 2), 10);
+  const timeMinute = dateStringSplit[1].slice(3, 5);
+  let time;
+  if(timeHour < 13) {
+    // Time is am
+    time = `${timeHour}:${timeMinute}am`;
+  } else {
+    // Time is pm
+    time = `${timeHour - 12}:${timeMinute}pm`
+  }
+ 
+  return `${date}, ${time}`;
+}
