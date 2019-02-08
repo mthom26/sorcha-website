af733745d01fa952b036662a09d788e26b28e7bd
mthom26 <mthom2626@gmail.com>
2019-02-08 10:32:24 +0000

    Add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,28 @@ const Footer = () => {
     <div className="footer">
       <div className="footerContainer">
         <div className="footerLogo">
-          
+          <a
+            href="https://www.contentful.com/"
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+          >
+            <img src="https://images.ctfassets.net/fo9twyrwpveg/7Htleo27dKYua8gio8UEUy/0797152a2d2f8e41db49ecbf1ccffdaa/PoweredByContentful_DarkBackground_MonochromeLogo.svg" style={{ maxWidth:"100px", width:"100%" }} alt="Powered by Contentful" />
+          </a>
         </div>
         <div className="footerContent">
           <div className="footerIcons">
             <a href="#"><img src={facebook} width="24px" alt="" /></a>
             <a href="#"><img src={youtube} width="24px" alt="" /></a>
           </div>
-          <div className="footerText">2018 Sorcha Thompson</div>
+          <div className="footerText">&copy; 2018 Sorcha Thompson</div>
+          <div className="footerText footerTextSmall">Website created by <a
+            href="https://www.michaelthompson.me/"
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+          >
+              Michael Thompson
+            </a>
+          </div>
         </div>
       </div>
     </div>
