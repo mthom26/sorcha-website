4c436e6a746e0a4dcb05ad05322813a0b7b0abc0
mthom26 <mthom2626@gmail.com>
2019-02-18 13:41:19 +0000

    Update translations

--- src/data/locales.js
+++ src/data/locales.js
@@ -12,7 +12,7 @@ module.exports = {
         contact: 'contact'
       },
       landing: {
-        content: 'Sorcha Thompson is a violinist living in Berlin. Although she grew up learning classical repertoire and playing in orchestras, her interests have since extended to genres as diverse as Irish folk and synth pop. She is active in several bands demonstrating the versatility of the violin as both melodic and rhythm instrument, as synthesiser and fiddle. No matter the musical context, Sorcha combines genre-specific techniques with her own musical ear to create an authentic, individual sound, whether on stage or in the recording studio.'
+        content: 'Violinist, Live Music, Studio Work'
       },
       bio: {
         title: 'About',
@@ -27,12 +27,12 @@ module.exports = {
         },
         vioLarkin: {
           title: 'Vio Larkin',
-          content: 'The string trio VioLarkin is an offshoot of Larkin.They play Celtic favourites and film music in new arrangements.',
+          content: 'The string trio VioLarkin is an offshoot of Larkin. They play Celtic favourites and film music in new arrangements.',
           link: 'http://www.larkin-music.de/'
         },
         synthPopTroubadours: {
           title: 'Synth Pop Troubadours',
-          content: "A showstopping, heart-thumping ride from start to finish, Synth Pop Troubadours will surprise you with their glittery blend of Art Trash and New Wave. Their shows combine playful synth beats and pop hooks with fabulous costumes, an experience that you're not likely to forget in a hurry.",
+          content: "Art Trash meets New Wave. Synth Pop Troubadours are the crazy, glittery musicians who have formed a band with keyboarder and songwriter Stephen Paul Taylor. Their shows combine playful synth beats and pop hooks with fabulous costumes, an experience that you're not likely to forget in a hurry.",
           link: 'https://stephenpaultaylor.net/'
         },
         movesJohnson: {
@@ -89,32 +89,32 @@ module.exports = {
         contact: 'kontact'
       },
       landing: {
-        content: 'Sorcha Thompson is a violinist living in Berlin. Although she grew up learning classical repertoire and playing in orchestras, her interests have since extended to genres as diverse as Irish folk and synth pop. She is active in several bands demonstrating the versatility of the violin as both melodic and rhythm instrument, as synthesiser and fiddle. No matter the musical context, Sorcha combines genre-specific techniques with her own musical ear to create an authentic, individual sound, whether on stage or in the recording studio.'
+        content: 'Violinistin, Live Musik, Studioaufnahmen'
       },
       bio: {
         title: 'Biografie',
-        content: 'Sorcha Thompson is a violinist living in Berlin. Although she grew up learning classical repertoire and playing in orchestras, her interests have since extended to genres as diverse as Irish folk and synth pop. She is active in several bands demonstrating the versatility of the violin as both melodic and rhythm instrument, as synthesiser and fiddle. No matter the musical context, Sorcha combines genre-specific techniques with her own musical ear to create an authentic, individual sound, whether on stage or in the recording studio.'
+        content: 'Sorcha ist eine professionelle Violistin mit Schaffensmittelpunkt in Berlin. Ihre Wurzeln liegen in der Klassik und im klassischen Orchester, was sie bis heute prägt. In den vergangenen Jahren hat sie ihr musikalisches Spektrum erweitert und bedient inzwischen unterschiedlichste Stilrichtungen von Irish Folk bis Synth Pop. Aktuell ist sie in mehreren Bands tätig, in denen sie ihre Geige vielseitig zum Einsatz bringt. Sei es als Solistin oder Begleitinstrument, als Fiddle im Irish Ensemble oder Synthesizerfläche mit der E-Geige. Sorcha verbindet genrespezifische Spieltechniken mit eigener, musikalischer Intuition, um, egal ob auf der Bühne oder im Aufnahmestudio, stets einen individuellen, authentischen Sound zu schaffen.'
       },
       music: {
         headerText: 'Bands',
         larkin: {
           title: 'Larkin',
-          content: 'Larkin are one of the longest running Irish folk bands in eastern Germany. The band was formed in Berlin in 1972 by the Scottish singer Jack Mitchell and today appears in various formations with a range of musicians. Brilliant musicianship is combined with poetic and often humorous German translations of Irish and Scottish folksongs.',
+          content: 'Larkin ist eine der dienstältesten Irish-Folk-Bands Ostdeutschlands. Sie wurde 1972 von dem schottischen Sänger Jack Mitchell als Studentenband mitgegründet und besteht heute aus mehreren MusikerInnen, die in verschiedenen Besetzungen spielen. Die brillante instrumentale Spielfähigkeit der Musiker wird verbunden mit der oft humorvollen und sehr poetischen deutschen Übersetzung irischer und schottischer Folksongs.',
           link: 'http://www.larkin-music.de/'
         },
         vioLarkin: {
           title: 'Vio Larkin',
-          content: 'The string trio VioLarkin is an offshoot of Larkin.They play Celtic favourites and film music in new arrangements.',
+          content: 'VioLarkin ist ein Streichertrio und Ableger von Larkin, das für eine abwechslungsreiche Mischung aus keltischen Folksongs und bekannten Titeln aus großer Filmmusik steht.',
           link: 'http://www.larkin-music.de/'
         },
         synthPopTroubadours: {
           title: 'Synth Pop Troubadours',
-          content: "A showstopping, heart-thumping ride from start to finish, Synth Pop Troubadours will surprise you with their glittery blend of Art Trash and New Wave. Their shows combine playful synth beats and pop hooks with fabulous costumes, an experience that you're not likely to forget in a hurry.",
+          content: "Art Trash trifft New Wave. Synth Pop Troubadours sind die verrückten und glitzernden MusikerInnen, die sich als Band um Stephen Paul Taylor zusammen getan haben. Ihre Musik steht für verspielte Synth Beats, poppige Melodien und eine Live-Show, die du nicht so schnell vergessen wirst.",
           link: 'https://stephenpaultaylor.net/'
         },
         movesJohnson: {
           title: 'Moves Johnson',
-          content: "Born in Canada, Moves currently lives in Berlin, Germany. He is best known for his resonant and often humorous lyrics, his aggressive performance style and his witty stage banter. A performer to the core, Moves is a musician on both the streets and the stage. Whether it's a crowded subway station, a bustling city street or a dimly lit and smokey bar, Moves finds the music to fit any occasion.",
+          content: "Moves is gebürtiger Kanadier. Derzeit lebt und arbeitet er in Berlin. Seine Shows spielen mit humorvollen Texten, seiner einnehmenden Bühnenpräsenz, markigen Sprüchen und Anekdoten. Moves lebt für die Performance. Sei es als Musiker auf der Straße, in überfüllten U- Bahnstationen oder in gemütlichen Kneipen. Er findet den richtigen Ton für jede Bühne dieser Stadt.",
           link: 'https://www.facebook.com/MovesJohnsonMusic/'
         }
       },
@@ -136,7 +136,7 @@ module.exports = {
         }
       },
       upcomingGigs: {
-        title: 'Kommende Konzerte'
+        title: 'Nächste Konzerte'
       },
       gallery: {
         headerText: 'Galerie'
@@ -145,8 +145,8 @@ module.exports = {
         headerText: 'Blog'
       },
       contact: {
-        headerText: 'Kontact',
-        content: 'If you would like to book a show, schedule a recording session or otherwise make contact, use the form below to send a message.',
+        headerText: 'Kontakt',
+        content: 'Anfragen und Buchungen für Live-Musik oder Recording nehme ich gern über das untenstehende Kontaktformular entgegen.',
         name: 'Name',
         email: 'Email',
         message: 'Nachricht'
